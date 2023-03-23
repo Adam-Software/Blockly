a408082ecf27e4cb4db8ffa6388bb1aeabfa22c0
Alexey Nesterov <vertigra.developer@gmail.com>
2023-03-24 09:53:23 +1000

    Fix: [JSC_MISSING_REQUIRE]

--- generators/python/system_call.js
+++ generators/python/system_call.js
@@ -11,7 +11,6 @@
 
 goog.module('Blockly.Python.system_call');
 
-const {NameType} = goog.require('Blockly.Names');
 const {pythonGenerator: Python} = goog.require('Blockly.Python');
 
 // If any new block imports any library, add that library name here.
@@ -19,17 +18,17 @@ Python.addReservedWords('os, subprocess, sh, ls, app, bno055_webgl.server');
 
 // systems_os_system
 Python['systems_os_system'] = function(block) {
-  Blockly.Python.definitions_['import_os'] = 'import os';
-  var value_param = Blockly.Python.valueToCode(block, 'param', Blockly.Python.ORDER_NONE);
+  Python.definitions_['import_os'] = 'import os';
+  var value_param = Python.valueToCode(block, 'param', Python.ORDER_NONE);
   var code = 'os.system(' + value_param + ')';
-  return [code, Blockly.Python.ORDER_NONE];
+  return [code, Python.ORDER_NONE];
 };
 
 // common_app_echo
 Python['systems_subprocess_call'] = function(block) {
-  Blockly.Python.definitions_['import_subprocess'] = 'import subprocess';
-  var value_param = Blockly.Python.valueToCode(block, 'param', Blockly.Python.ORDER_NONE);
+  Python.definitions_['import_subprocess'] = 'import subprocess';
+  var value_param = Python.valueToCode(block, 'param', Python.ORDER_NONE);
   var checkbox_shell = (block.getFieldValue('shell') == 'TRUE') ? 'True' : 'False';
   var code = 'subprocess.call(' + value_param + ', ' + 'shell=' + checkbox_shell +')';
-  return [code, Blockly.Python.ORDER_NONE];
+  return [code, Python.ORDER_NONE];
 };
